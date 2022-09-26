81e086950a1fddd9d87a801f7ad5cf6ef6a9ea38
syket-das <saiketsd23@gmail.com>
2022-09-26 09:36:46 +0530

    categories --prototype

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import LayoutComponent from '../components/Layout';
-import { Table, Tag, Space, Row, Button } from 'antd';
+import { Table, Tag, Space, Row, Button, Col, Input, Breadcrumb, Empty } from 'antd';
 import {
   EditTwoTone,
   DeleteTwoTone,
@@ -8,6 +8,11 @@ import {
   MinusCircleTwoTone,
 } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeNavigationHighlight, changeNavigationLink } from '../redux/layout/navigationSlice';
+
+const { TextArea } = Input;
+
 const columns = [
   {
     title: 'Name',
@@ -110,24 +115,63 @@ const data = [
 ];
 
 const Categories = () => {
+  const dispatch = useDispatch();
+  // const current = useSelector((state) => state.navigation.hightlight);
+  // const link = useSelector((state) => state.navigation.link);
+
   return (
     <LayoutComponent>
       <div className="p-5">
         <Row justify="space-between" className="mb-5">
-          <h1>Categories</h1>
           <div>
-            <Button type="primary" size='lg' ghost className="py-2.5">
+            <h1>Categories</h1>
+
+            <div className="mt-1">
+              <Breadcrumb>
+                <Breadcrumb.Item>
+                  <Link
+                    to="/"
+                    onClick={() => {
+                      dispatch(changeNavigationHighlight('1'));
+                      dispatch(changeNavigationLink('/'));
+                    }}
+                  >
+                    Home
+                  </Link>
+                </Breadcrumb.Item>
+                <Breadcrumb.Item>
+                  <Link to="/categories">Categories</Link>
+                </Breadcrumb.Item>
+              </Breadcrumb>
+            </div>
+          </div>
+          <div>
+            <Button type="primary" ghost className="py-2.5">
               Add Category
             </Button>
           </div>
         </Row>
         <Row>
-          <Table
-            id="box"
-            columns={columns}
-            dataSource={data}
-            scroll={{ y: 300 }}
-          />
+          <Col lg={12} md={12} sm={24} className="p-2 ">
+            <Table
+              id="box"
+              columns={columns}
+              dataSource={data}
+              scroll={{ y: 300 }}
+              style={{ height: '400px' }}
+            />
+          </Col>
+          <Col lg={12} md={12} sm={24} className="p-2 ">
+            <TextArea
+              rows={4}
+              placeholder="maxLength is 6"
+              maxLength={6}
+              disabled
+              style={{ height: '355px' }}
+            >
+            </TextArea>
+            
+          </Col>
         </Row>
       </div>
     </LayoutComponent>
